afb6b4d3077de4ef900e1beec0f42bbe4ad36352
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-06-01 18:44:58 -0300

    fix: changes in the database connection module

--- backend/src/config/dbConnect.ts
+++ backend/src/config/dbConnect.ts
@@ -1,6 +1,16 @@
 import "dotenv/config";
 import mongoose from "mongoose";
-if (process.env.URI != undefined) mongoose.connect(process.env.URI);
-const db = mongoose.connection;
-export default db;
+
+async function connect(){
+    try{
+        mongoose.set("strictQuery", true);
+        if (process.env.URI != undefined) await mongoose.connect(process.env.URI);
+        console.log("Connected database");
+        
+    } catch(err){
+        console.log(err);
+    }
+}
+
+export default connect;
 
