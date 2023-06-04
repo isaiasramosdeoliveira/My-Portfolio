35ae3721498ee056f6c7541ab5cdad6e442ff07e
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-06-04 00:24:27 -0300

    feat: initialized nest settings

--- backend/src/routes/courses.ts
+++
@@ -1,9 +0,0 @@
-import express, {Router} from "express";
-import CourseController from "../controllers/CourseController";
-
-const route: Router = express.Router();
-route
-    .get("/courses", CourseController.getCourses);
-
-
-export default route;
